3c35d35c37c4dc615646db563e69905be86fed6e
Will Moeller <willmoeller@gmail.com>
2023-03-04 15:19:23 +0000

    Saved changes.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -11,24 +11,23 @@
 #  updated_at :datetime         not null
 #
 class Actor < ApplicationRecord
-  def characters
-    key = self.id
+  has_many(:characters, { :class_name => "Character", :foreign_key => "actor_id" }) # direct relationship
+  # def characters
+  #   key = self.id
+  #   the_many = Character.where({ :actor_id => key })
+  #   return the_many
+  # end
 
-    the_many = Character.where({ :actor_id => key })
+  has_many(:filmography, { :through => :characters, :source => :movie }) # intermediate relationship
+  # many to many
+  # def filmography # indirect relationship
+  #   the_many = Array.new
+  #   self.characters.each do |joining_record|
+  #     destination_record = joining_record.movie
+  #     the_many.push(destination_record)
+  #   end
+  #   return the_many
+  # end
 
-    return the_many
-  end
-
-  def filmography
-    the_many = Array.new
-
-    self.characters.each do |joining_record|
-      destination_record = joining_record.movie
-
-      the_many.push(destination_record)
-    end
-
-    return the_many
-  end
-
+  has_many(:directors, { :through => :filmography, :source => :director })
 end
